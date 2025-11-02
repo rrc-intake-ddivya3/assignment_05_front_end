17eb28f9a3fabdf28d04b2eb8092a129a28d3109
divya sharma <sddivyasharma2005@gmail.com>
2025-11-02 01:53:41 -0600

    fetching data and the url

--- script.js
+++ script.js
@@ -6,10 +6,19 @@ const apiUrl = `https://data.winnipeg.ca/api/v3/views/hfwk-jp4h/query.json?$wher
 async function fetchTreeData(treeName){
     const apiUrl = `https://data.winnipeg.ca/api/v3/views/hfwk-jp4h/query.json?$where=commonname='${treeName}%'&$order=diametre_at_breast_height DESC&$limit=100`;
     try{
+        // GET request to the API
         const response = await fetch(apiUrl);
+
+        // Check if the response is ok
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        const data = await response.json();
+        console.log(data);
+
+        const resultsDiv = document.getElementById('results');
+        resultsDiv.innerHTML = ''; // Clear previous results
     }
 
 } 
